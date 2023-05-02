2d5789f514c9d23c5519955223840def044e8973
Antonella Tissandie <antonellatissandie@hotmail.com>
2023-05-01 22:15:13 -0300

    Entrega

---
+++ App1/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+ 
+class CursoFormulario(forms.Form):
+    id= forms.IntegerField()
+    nombre = forms.CharField()
+    curso = forms.IntegerField()
+
+class ProfesorFormulario(forms.Form):
+    id= forms.IntegerField()
+    nombre= forms.CharField(max_length=30)
+    apellido= forms.CharField(max_length=30)
+    email= forms.EmailField()
+    profesion= forms.CharField(max_length=30)
+
+class EstudiantesFormulario(forms.Form):
+    id= forms.IntegerField()
+    nombre = forms.CharField()
+    curso = forms.IntegerField()
